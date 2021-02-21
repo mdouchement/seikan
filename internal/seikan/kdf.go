f758b0359940c0d5e6d891503d40ff440f096e98
mdouchement <marc.douchement@gmail.com>
2021-02-21 15:40:26 +0100

    0.1.2: Fix panic with go1.16

--- internal/seikan/kdf.go
+++ internal/seikan/kdf.go
@@ -23,26 +23,32 @@ func KDFGenerate(s string) ([]byte, error) {
 		return nil, err
 	}
 
-	h, err := blake2b.New256(payload[:saltlength])
-	if err != nil {
-		return nil, err
+	nash := func() hash.Hash {
+		h, err := blake2b.New256(payload[:saltlength])
+		if err != nil {
+			panic(err)
+		}
+		return h
 	}
 
-	kdf := hkdf.New(func() hash.Hash { return h }, []byte(s), nil, nil)
-	_, err = io.ReadFull(kdf, payload[saltlength:])
+	kdf := hkdf.New(nash, []byte(s), nil, nil)
+	_, err := io.ReadFull(kdf, payload[saltlength:])
 	return payload, err
 }
 
 // KDFCompare returns true if the hased payload match the given s.
 func KDFCompare(payload []byte, s string) bool {
-	h, err := blake2b.New256(payload[:saltlength])
-	if err != nil {
-		panic(err)
+	nash := func() hash.Hash {
+		h, err := blake2b.New256(payload[:saltlength])
+		if err != nil {
+			panic(err)
+		}
+		return h
 	}
-	kdf := hkdf.New(func() hash.Hash { return h }, []byte(s), nil, nil)
+	kdf := hkdf.New(nash, []byte(s), nil, nil)
 
 	hash := make([]byte, KDFLength-saltlength)
-	_, err = io.ReadFull(kdf, hash)
+	_, err := io.ReadFull(kdf, hash)
 	if err != nil {
 		panic(err)
 	}
